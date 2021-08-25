34fda40db821e71f87ab1aac4a24455c9a3a2786
redshift14 <anasarif.mi@gmail.com>
2021-08-25 14:15:40 +0100

    final-2

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use('/auth', require('./routers/userRouter'));
 app.use('/users', require('./routers/protectedRoutes'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   })
 }
 else {
